9abf719cd84dd74eeff8f9aae08c97f2c19977c1
Shirley <xiaochunyan0218@126.com>
2022-05-04 21:18:16 +0800

    live

---
+++ taoBaoLive/data.mjs
@@ -0,0 +1,68 @@
+var totalData = {
+  startTime: 0,
+  durationTime: 0,
+  dataRefreshTime: 0,
+  liveAudience: 121,
+  acitve: 0.405,
+  saleAmout: 9096,
+  saleAmoutNoNonfansRate: 0.285,
+  watchTimePerPerson: 65,
+  watchTimePerPersonNonfans: 28,
+  commodityClick: 0.264,
+  commodityClickNonfans: 0.132,
+  dealTransitionRate: 0.008,
+  dealTransitionRateNonfans: 0.0011,
+}
+
+var retailData = {
+  No1: {
+    commodityDecription: { id: 112, picURL: './112.png', title: '梅子熟了中式复古旗袍改良碎花裙子女夏装什么什么什么什么什么' },
+    liveAudience: 18,
+    exposed: 9026,
+    click: 1987,
+    addToCart: 54,
+    deal: 17,
+    saleAmount: 3073.6
+  },
+  No2: {
+    commodityDecription: { id: 111, picURL: './111.png', title: '梅子熟了吊带长裙仙女荷叶边印花雪纺裙子什么什么什么什么什么' },
+    liveAudience: 18,
+    exposed: 7906,
+    click: 1474,
+    addToCart: 36,
+    deal: 5,
+    saleAmount: 1155
+  },
+  No3: {
+    commodityDecription: { id: 110, picURL: './110.png', title: '梅子熟了法式碎花连衣裙圆领长袖长裙高什么什么什么什么什么什么' },
+    liveAudience: 18,
+    exposed: 7305,
+    click: 1287,
+    addToCart: 23,
+    deal: 3,
+    saleAmount: 627
+  },
+  No4: {
+    commodityDecription: { id: 57, picURL: './57.png', title: '梅子熟了法式温柔碎花裙荷叶V领高腰长袖连衣裙什么什么什么什么什么' },
+    liveAudience: 13,
+    exposed: 671,
+    click: 54,
+    addToCart: 5,
+    deal: 3,
+    saleAmount: 582
+  },
+  N05: {
+    commodityDecription: { id: 64, picURL: './64.png', title: '梅子熟了+清新绿H版型气质西装反驳领垫肩什么什么什么什么什么什么' },
+    liveAudience: 13,
+    exposed: 723,
+    click: 48,
+    addToCart: 1,
+    deal: 2,
+    saleAmount: 488
+  },
+}
+
+
+
+
+export { totalData, retailData }
